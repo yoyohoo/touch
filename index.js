aa0f8922ddfe2cdd1911e048318a9b67a94f2778
yoyohoo <sinful_001@sina.com>
2017-12-13 19:12:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,130 @@
 window.onload = function () {
     setTimeout(scrollTo, 0, 0, 0);
+
+    var mTouch = {
+        init: function () {
+            var evt = localStorage.opType;
+            if (!evt) return false;
+            this[evt].call();
+        },
+        tap: function () {
+            /*
+             * tap
+             */
+
+            touch.on(target, 'tap', function (e) {
+                if (!touch.config.tap) return false;
+                result.innerHTML = '你敲击了图片一下';
+            });
+        },
+        doubleTap: function () {
+            /*
+             * doubleTap
+             */
+            touch.on(target, 'doubleTap', function (e) {
+                if (!touch.config.doubleTap) return false;
+                result.innerHTML = '你连续敲击了图片两下';
+            });
+        },
+        hold: function () {
+            /*
+             * hold
+             */
+            touch.on(target, 'hold', function (e) {
+                if (!touch.config.hold) return false;
+                result.innerHTML = '你按住了图片';
+            });
+            touch.on(target, 'touchend', function (e) {
+                if (!touch.config.hold) return false;
+                result.innerHTML = '你放开了图片';
+            });
+        },
+        rotate: function () {
+            /*
+             * rotate
+             */
+            var angle = 0;
+            touch.on(target, 'touchstart', function (ev) {
+                ev.startRotate();
+                ev.preventDefault();
+            });
+            touch.on(target, 'rotate', function (ev) {
+                if (!touch.config.rotate) return false;
+                var totalAngle = angle + (ev.rotation || 0);
+                if (ev.fingerStatus === 'end') {
+                    angle = angle + ev.rotation;
+                }
+                result.innerHTML = '你旋转了：' + angle + '°';
+                this.style.webkitTransform = 'rotate(' + totalAngle + 'deg)';
+                angle = 0;
+            });
+        },
+        scale: function () {
+            /*
+             * scale
+             */
+            touch.on(target, 'touchstart', function (e) {
+                e.preventDefault();
+            });
+            var initialScale = 1;
+            var currentScale;
+            touch.on(target, 'pinchend', function (e) {
+                if (!touch.config.scale) return false;
+                currentScale = e.pinch - 1;
+                currentScale = initialScale + currentScale;
+                currentScale = currentScale > 2 ? 2 : currentScale;
+                currentScale = currentScale < 1 ? 1 : currentScale;
+                this.style.webkitTransform = 'scale(' + currentScale + ')';
+                result.innerHTML = "当前缩放比例为:" + currentScale + ".";
+            });
+            touch.on(target, 'pinchend', function (e) {
+                if (!touch.config.scale) return false;
+                initialScale = currentScale;
+            });
+        },
+        drag: function () {
+            /*
+             * drag
+             */
+            touch.on(target, 'touchstart', function (e) {
+                e.preventDefault();
+            });
+            var dx, dy;
+            touch.on(target, 'drag', function (e) {
+                if (!touch.config.drag) return false;
+                dx = dx || 0;
+                dy = dy || 0;
+                var offx = dx + e.x + "px";
+                var offy = dy + e.y + "px";
+                this.style.webkitTransform = "translate3d(" + offx + "," + offy + ",0)";
+            });
+            touch.on(target, 'dragend', function (e) {
+                if (!touch.config.drag) return false;
+                dx += e.x;
+                dy += e.y;
+            });
+        },
+        swipe: function () {
+            /*
+             * swipe
+             */
+            document.getElementById('tch-img').style.webkitTransition = 'all ease 1s';
+            touch.on(target, 'touchstart', function (ev) {
+                ev.preventDefault();
+            });
+            touch.on(target, 'swiperight', function (ev) {
+                if (!touch.config.swipe) return false;
+                this.style.webkitTransform = "translate3d(" + this.offsetLeft + "px,0,0)";
+                result.innerHTML = "向右滑动：" + this.offsetLeft;
+            });
+            touch.on(target, 'swipeleft', function (ev) {
+                if (!touch.config.swipe) return false;
+                this.style.webkitTransform = "translate3d(-" + this.offsetLeft + "px,0,0)";
+                result.innerHTML = "向左滑动：" + this.offsetLeft;
+            });
+        }
+    }
+
     var target = '#tch-img',
         img = document.getElementById('tch-img'),
         event = document.getElementById('tch-event'),
@@ -21,6 +146,8 @@ window.onload = function () {
     }
     reset();
 
+    touch.on('#tch-reload a', 'tap', reload)
+
     touch.on('.tch-ctrl', 'tap', function (e) {
         reset();
         if (e.target.tagName !== 'A') return;
@@ -33,102 +160,22 @@ window.onload = function () {
         result.innerHTML = '';
         // angle += Math.floor(Math.random() * 90)
         // touch.trigger(img, evt)
+        localStorage.opType = evt;
+        mTouch.init()
     })
 
     /*
-     * tap
+     * reset
      */
-    touch.on(target, 'tap', function (e) {
-        if (!touch.config.hold) return false;
-        result.innerHTML = '你敲击了图片一下';
-    });
-
-    /*
-     * doubleTap
-     */
-    touch.on(target, 'doubleTap', function (e) {
-        if (!touch.config.doubleTap) return false;
-        result.innerHTML = '你连续敲击了图片两下';
-    });
-
-    /*
-     * hold
-     */
-    touch.on(target, 'hold', function (e) {
-        if (!touch.config.hold) return false;
-        result.innerHTML = '你按住了图片';
-    });
-    touch.on(target, 'touchend', function (e) {
-        if (!touch.config.hold) return false;
-        result.innerHTML = '你放开了图片';
-    });
-
-    /*
-     * rotate
-     */
-    // var angle = 0;
-    // touch.on(target, 'touchstart', function (ev) {
-    //     ev.startRotate();
-    //     ev.preventDefault();
-    // });
-    // touch.on(target, 'rotate', function (ev) {
-    //     if (!touch.config.rotate) return false;
-    //     var totalAngle = angle + (ev.rotation || 0);
-    //     if (ev.fingerStatus === 'end') {
-    //         angle = angle + ev.rotation;
-    //     }
-    //     result.innerHTML = '你旋转了：' + angle + '°';
-    //     this.style.webkitTransform = 'rotate(' + totalAngle + 'deg)';
-    //     angle = 0;
-    // });
-
-
-    /*
-     * scale
-     */
-    touch.on(target, 'touchstart', function (e) {
-        e.preventDefault();
-    });
-    var initialScale = 1;
-    var currentScale;
-    touch.on(target, 'pinchend', function (e) {
-        if (!touch.config.scale) return false;
-        currentScale = e.pinch - 1;
-        currentScale = initialScale + currentScale;
-        currentScale = currentScale > 2 ? 2 : currentScale;
-        currentScale = currentScale < 1 ? 1 : currentScale;
-        this.style.webkitTransform = 'scale(' + currentScale + ')';
-        result.innerHTML = "当前缩放比例为:" + currentScale + ".";
-    });
-    touch.on(target, 'pinchend', function (e) {
-        if (!touch.config.scale) return false;
-        initialScale = currentScale;
-    });
-
-    /*
-     * drag
-     */
-    // touch.on(target, 'touchstart', function (e) {
-    //     e.preventDefault();
-    // });
-    // var dx, dy;
-    // touch.on(target, 'drag', function (e) {
-    //     if (!touch.config.drag) return false;
-    //     dx = dx || 0;
-    //     dy = dy || 0;
-    //     var offx = dx + e.x + "px";
-    //     var offy = dy + e.y + "px";
-    //     this.style.webkitTransform = "translate3d(" + offx + "," + offy + ",0)";
-    // });
-    // touch.on(target, 'dragend', function (e) {
-    //     if (!touch.config.drag) return false;
-    //     dx += e.x;
-    //     dy += e.y;
-    // });
-
-
     function reset() {
         touch.config = config;
+    }
+
+    /*
+     * reload
+     */
+    function reload() {
+        location.reload();
     }
 
 }
