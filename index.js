4facb95c85c3f065e3d6a9eef3e88501c14f50f6
yoyohoo <sinful_001@sina.com>
2017-12-13 17:30:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 window.onload = function () {
     setTimeout(scrollTo, 0, 0, 0);
     var target = '#tch-img',
+        img = document.getElementById('tch-img'),
         event = document.getElementById('tch-event'),
         result = document.getElementById('tch-result');
 
@@ -9,6 +10,7 @@ window.onload = function () {
         doubleTap: false, //doubleTap事件开关， 默认为true
         hold: false, //hold事件开关, 默认为true
         holdTime: 650, //hold时间长度
+        rotate: false,
         swipe: false, //swipe事件开关
         swipeTime: 300, //触发swipe事件的最大时长
         swipeMinDistance: 18, //swipe移动最小距离
@@ -26,16 +28,20 @@ window.onload = function () {
         touch.config[evt] = true;
         event.innerText = '开启' + evt + '事件：';
         result.innerHTML = '';
-        angle += Math.floor(Math.random() * 90)
-        touch.trigger(document.getElementById('tch-img'), evt)
+        // angle += Math.floor(Math.random() * 90)
+        // touch.trigger(img, evt)
     })
 
+    /*
+     * rotate
+     */
     var angle = 0;
     touch.on(target, 'touchstart', function (ev) {
         ev.startRotate();
         ev.preventDefault();
     });
     touch.on(target, 'rotate', function (ev) {
+        if (!touch.config.rotate) return false;
         var totalAngle = angle + (ev.rotation || 0);
         if (ev.fingerStatus === 'end') {
             angle = angle + ev.rotation;
@@ -46,6 +52,25 @@ window.onload = function () {
     });
 
 
+    /*
+     * scale
+     */
+    touch.on(target, 'touchstart', function (ev) {
+        ev.preventDefault();
+    });
+    var initialScale = 1;
+    var currentScale;
+    touch.on(target, 'pinchend', function (ev) {
+        currentScale = ev.scale - 1;
+        currentScale = initialScale + currentScale;
+        currentScale = currentScale > 2 ? 2 : currentScale;
+        currentScale = currentScale < 1 ? 1 : currentScale;
+        this.style.webkitTransform = 'scale(' + currentScale + ')';
+        result.innerHTML = "当前缩放比例为:" + currentScale;
+    });
+    touch.on(target, 'pinchend', function (ev) {
+        initialScale = currentScale;
+    });
 
 
 }
