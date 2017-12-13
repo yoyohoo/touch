2b4d7792c03ee4105ee477d66067dc17cb30d1dc
yoyohoo <sinful_001@sina.com>
2017-12-13 15:09:20 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 window.onload = function () {
-    var target = '#tch-img';
+    var target = '#tch-img',
+        btns = document.querySelectorAll('.tch-ctrl li'),
+        info = document.getElementById('tch-result');
     touch.config = {
         tap: false, //tap类事件开关, 默认为true
-        doubleTap: true, //doubleTap事件开关， 默认为true
-        hold: true, //hold事件开关, 默认为true
+        doubleTap: false, //doubleTap事件开关， 默认为true
+        hold: false, //hold事件开关, 默认为true
         holdTime: 650, //hold时间长度
-        swipe: true, //swipe事件开关
+        swipe: false, //swipe事件开关
         swipeTime: 300, //触发swipe事件的最大时长
         swipeMinDistance: 18, //swipe移动最小距离
         swipeFactor: 5, //加速因子, 值越大变化速率越快
-        drag: true, //drag事件开关
-        pinch: true, //pinch类事件开关
+        drag: false, //drag事件开关
+        pinch: false, //pinch类事件开关
     }
 
-    touch.on(target, 'tap', function (e) {
-        console.info(e)
+    document.querySelector('.tch-ctrl').addEventListener('click', function (e) {
+        if (e.target.tagName !== 'A') return;
+        document.querySelector('.active').className = '';
+        e.target.className = 'active';
+        var evt = e.target.innerText;
+        info.innerText = '开启' + evt + '事件：';
+        touch.on(target, evt, function (e) {
+            alert(evt)
+            console.info(evt)
+        })
     })
-
-    touch.on(target, 'doubleTap', function (e) {
-        console.info(e)
-    })
-
 }
 
 function rotate() {
